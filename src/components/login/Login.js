e8c876646ba5409d17c5d76779508f3448cd29bf
phanison898 <phanison898@gmail.com>
2021-01-22 01:40:49 +0530

    updated login page

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,50 +1,77 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import { LoginAction } from "../../store/actions/auth";
-import { auth, provider } from "../../firebase";
-import TextLogo from "../../images/fb_text.png";
-import Logo from "../../images/logo.png";
-import { FacebookBlue } from "../../assets/Colors";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import { Paper } from "@material-ui/core";
+import firebase from "firebase";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import Logo from "./../../images/fb_text.png";
+import Style from "./Style";
 
 const Login = () => {
   const classes = Style();
-  const dispatch = useDispatch();
-  const login = () => {
-    auth
-      .signInWithPopup(provider)
-      .then((result) => {
-        dispatch(LoginAction(result.user));
-      })
-      .catch((error) => alert(error.message));
+
+  const uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
   };
+
   return (
-    <div className={classes.login}>
-      <img src={Logo} style={{ height: 200 }} alt="facebook-logo" />
-      <img src={TextLogo} style={{ width: 100 }} alt="facebook-text-logo" />
-      <Button onClick={login}>Login with Google</Button>
+    <div className={classes.login__container}>
+      <Paper elevation={3} className={classes.login}>
+        <div className={classes.logo}>
+          <img src={Logo} alt="linked-in-logo" />
+          <h4>Clone</h4>
+        </div>
+        <form className={classes.form} onSubmit={(e) => e.preventDefault()}>
+          <input disabled type="email" value="" placeholder="email" />
+          <input disabled type="password" value="" placeholder="password" />
+          <button disabled>Log In</button>
+        </form>
+        <div className={classes.google}>
+          <section>
+            <div></div>
+            <p>OR</p>
+            <div></div>
+          </section>
+          <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+        </div>
+        <div className={classes.about}>
+          <section>
+            <div></div>
+            <p>contact author</p>
+            <div></div>
+          </section>
+          <div>
+            {author.map(({ src, url, color }, i) => (
+              <a
+                href={`${url}`}
+                key={`author-link-${i}`}
+                rel="noreferrer nofollow"
+                target="_blank"
+                style={{ color: color }}
+              >
+                {src}
+              </a>
+            ))}
+          </div>
+        </div>
+      </Paper>
     </div>
   );
 };
 
-const Style = makeStyles((theme) => ({
-  login: {
-    width: "100vw",
-    height: "100vh",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    "& > *": {
-      margin: 20,
-    },
-    "& >.MuiButtonBase-root": {
-      backgroundColor: FacebookBlue,
-      color: "white",
-      transition: "all 0.3s ease",
-    },
+const author = [
+  { src: <GitHubIcon />, url: "https://github.com/phanison898", color: "black" },
+  { src: <LinkedInIcon />, url: "https://www.linkedin.com/in/phanison225/", color: "#0057ae" },
+  {
+    src: <YouTubeIcon />,
+    url: "https://www.youtube.com/channel/UC4FAldAo2Ow_2F447yggcqA",
+    color: "red",
   },
-}));
+  { src: <InstagramIcon />, url: "https://www.instagram.com/phanison225/", color: "#b7066e" },
+  { src: <TwitterIcon />, url: "https://twitter.com/phanison225", color: "rgb(29 161 242)" },
+];
 
 export default Login;
---
+++ src/components/login/Style.js
@@ -0,0 +1,153 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { FacebookBlue, darkSecondary, darkPrimary } from "../../assets/Colors";
+
+export default makeStyles((theme) => ({
+  login__container: {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  login: {
+    minWidth: 400,
+    maxWidth: 400,
+    height: 500,
+    [theme.breakpoints.down("xs")]: {
+      minWidth: "90%",
+      maxWidth: "90%",
+      height: "90%",
+    },
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    "& > *": {},
+  },
+  "& > .firebaseui-idp-button": {
+    width: 200,
+  },
+  logo: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+    "& > img": {
+      height: 35,
+    },
+    "& > h4": {
+      display: "none",
+      color: "#0057ae",
+      fontSize: 40,
+      fontWeight: 800,
+      marginLeft: 10,
+    },
+  },
+
+  form: {
+    width: 250,
+    height: 200,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    "& > input": {
+      outlineWidth: 0,
+      height: 35,
+      border: "1px solid lightgrey",
+      borderRadius: 2,
+      padding: "0 10px",
+    },
+    "& > button": {
+      height: 30,
+      border: "1px solid lightgrey",
+      borderRadius: 4,
+      color: "white",
+      fontSize: 14,
+      fontWeight: 600,
+      backgroundColor: FacebookBlue,
+    },
+  },
+
+  google: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    "& > section": {
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: "0 10px",
+      "& > div": {
+        flex: 1,
+        height: 1,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "lightgrey",
+        opacity: 0.5,
+      },
+      "& > p": {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "0 10px",
+        fontSize: 12,
+        color: "grey",
+      },
+    },
+  },
+
+  about: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    "& > section": {
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: "0 10px",
+      marginBottom: 5,
+      "& > div": {
+        flex: 1,
+        height: 1,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "lightgrey",
+        opacity: 0.5,
+      },
+      "& > p": {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "0 10px",
+        fontSize: 10,
+        color: "grey",
+      },
+    },
+    "& > div": {
+      flex: 1,
+      width: "100%",
+      display: "flex",
+      justifyContent: "space-evenly",
+      alignItems: "center",
+      margin: "4px 0",
+      "& > a": {
+        color: "grey",
+        transition: "all 0.4s ease",
+        "&:hover": {
+          color: theme.palette.type === "dark" ? darkSecondary : darkPrimary,
+        },
+        "& > .MuiSvgIcon-root": {
+          fontSize: 18,
+        },
+      },
+    },
+  },
+}));
